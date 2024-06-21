2604b267316206f6ff822ac396c1fe333c652741
Ray muiruri <muiruriray9@gmail.com>
2024-06-21 15:40:25 +0300

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+# Assign values to variables
+number1 = 10
+number2 = 5
+
+
+# Perform operations
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print results in human-readable format
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
